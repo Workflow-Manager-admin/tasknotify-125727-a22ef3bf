740f294abfdce4d8511c2bd36ac3bab35bb89188
Gerardo Rios <gerardo@kavia.ai>
2025-05-26 14:31:19 -0500

    Auto-commit: Changes as of 2025-05-26T19:31:19.637346+00:00

---
+++ .linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /tmp/kavia/workspace/code-generation/tasknotify-125727-a22ef3bf/tasknotify
+flutter analyze
+
---
+++ tasknotify/android/app/src/main/kotlin/com/example/tasknotify/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tasknotify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
